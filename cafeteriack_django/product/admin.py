8a2862552291314cf73eff79f53594167ac8f22e
A1insr <164562181+A1insr@users.noreply.github.com>
2024-06-26 11:33:45 +0330

    Add files via upload
    Vertical instead of category
    Latest products instead of 12 best-selling products
    Added 3 verticals

---
+++ cafeteriack_django/product/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Vertical, Product
+
+admin.site.register(Vertical)
+admin.site.register(Product)
---
+++ cafeteriack_django/product/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+
+from .models import Vertical, Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "description",
+            "price",
+            "get_image",
+            "get_thumbnail"
+        )
+
+
+class VerticalSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+
+    class Meta:
+        model = Vertical
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "products",
+        )
