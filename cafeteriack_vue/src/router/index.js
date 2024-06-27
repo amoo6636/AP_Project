374bad8070b7960426f5bf093113e186b05bd1cb
sbyousuf <sbyousef83@gmail.com>
2024-06-27 13:31:08 +0430

    SignUp

--- cafeteriack_vue/src/router/index.js
+++ cafeteriack_vue/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 import Product from '../views/Product.vue'
+import SignUp from '../views/SignUp.vue'
 const routes = [
   {
     path: '/',
@@ -16,6 +17,17 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
+  {
+    path: '/:vertical_slug/:product_slug/',
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  
   {
     path: '/:vertical_slug/:product_slug/',
     name: 'Product',
