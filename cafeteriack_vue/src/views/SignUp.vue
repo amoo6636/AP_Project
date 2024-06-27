94bc8d627bda7886d549f76965cc142b3ad2f790
sbyousuf <sbyousef83@gmail.com>
2024-06-27 20:22:51 +0430

    modify

--- cafeteriack_vue/src/views/SignUp.vue
+++ cafeteriack_vue/src/views/SignUp.vue
@@ -3,32 +3,29 @@
         <div class="columns">
             <div class="column is-4 is-offset-4">
                 <h1 class="title">Sign up</h1>
-            
+
                 <form @submit.prevent="submitForm">
                     <div class="field">
-                        <label>name</label>
+                        <label>Name</label>
                         <div class="control">
                             <input type="text" class="input" v-model="name">
                         </div>
                     </div>
+
                     <div class="field">
                         <label>Username</label>
                         <div class="control">
                             <input type="text" class="input" v-model="username">
                         </div>
                     </div>
+
                     <div class="field">
-                        <label>email</label>
+                        <label>Email</label>
                         <div class="control">
-                            <input type="text" class="input" v-model="email">
+                            <input type="email" class="input" v-model="email">
                         </div>
                     </div>
-                    <div class="field">
-                        <label>Phone Number</label>
-                        <div class="control">
-                            <input type="tel" class="input" v-model="phoneNumber">
-                        </div>
-                    </div>
+
                     <div class="field">
                         <label>Password</label>
                         <div class="control">
@@ -43,8 +40,15 @@
                         </div>
                     </div>
 
+                    <div class="field">
+                        <label>Phone Number</label>
+                        <div class="control">
+                            <input type="tel" class="input" v-model="phoneNumber">
+                        </div>
+                    </div>
+
                     <div class="notification is-danger" v-if="errors.length">
-                        <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+                        <p v-for="error in errors" :key="error">{{ error }}</p>
                     </div>
 
                     <div class="field">
@@ -63,40 +67,62 @@
 </template>
 
 <script>
-import axios from 'axios'
-import { toast } from 'bulma-toast'
+import axios from 'axios';
+import { toast } from 'bulma-toast';
 
 export default {
     name: 'SignUp',
     data() {
         return {
+            name: '',
             username: '',
+            email: '',
             password: '',
             password2: '',
+            phoneNumber: '',
             errors: []
-        }
+        };
     },
     methods: {
         submitForm() {
-            this.errors = []
+            this.errors = [];
+
+            if (this.name === '') {
+                this.errors.push('The name is missing');
+            }
 
             if (this.username === '') {
-                this.errors.push('The username is missing')
+                this.errors.push('The username is missing');
+            }
+
+            if (this.email === '') {
+                this.errors.push('The email is missing');
+            } else if (!this.validateEmail(this.email)) {
+                this.errors.push('Invalid email address');
             }
 
             if (this.password === '') {
-                this.errors.push('The password is too short')
+                this.errors.push('The password is too short');
             }
 
             if (this.password !== this.password2) {
-                this.errors.push('The passwords doesn\'t match')
+                this.errors.push('The passwords don\'t match');
+            }
+
+            if (!this.phoneNumber) {
+                this.errors.push('Phone number required.');
+            } else if (!this.validatePhoneNumber(this.phoneNumber)) {
+                this.errors.push('Invalid phone number.');
             }
 
             if (!this.errors.length) {
                 const formData = {
+                    name: this.name,
                     username: this.username,
-                    password: this.password
-                }
+                    email: this.email,
+                    password: this.password,
+                    phoneNumber: this.phoneNumber
+                };
 
                 axios
                     .post("/api/v1/users/", formData)
@@ -108,25 +134,35 @@ export default {
                             pauseOnHover: true,
                             duration: 2000,
                             position: 'bottom-right',
-                        })
+                        });
 
-                        this.$router.push('/log-in')
+                        this.$router.push('/log-in');
                     })
                     .catch(error => {
                         if (error.response) {
                             for (const property in error.response.data) {
-                                this.errors.push(`${property}: ${error.response.data[property]}`)
+                                this.errors.push(`${property}: ${error.response.data[property]}`);
                             }
 
-                            console.log(JSON.stringify(error.response.data))
+                            console.log(JSON.stringify(error.response.data));
                         } else if (error.message) {
-                            this.errors.push('Something went wrong. Please try again')
+                            this.errors.push('Something went wrong. Please try again');
                             
-                            console.log(JSON.stringify(error))
+                            console.log(JSON.stringify(error));
                         }
-                    })
+                    });
             }
+        },
+        validatePhoneNumber(phoneNumber) {
+            // Simple phone number validation (you can improve it based on your requirements)
+            const phoneRegex = /^\+?[1-9]\d{1,14}$/;
+            return phoneRegex.test(phoneNumber);
+        },
+        validateEmail(email) {
+            // Simple email validation (you can improve it based on your requirements)
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email);
         }
     }
-}
+};
 </script>
